2aa3cf597356e762668f635bab5a3b2c0a84654c
[gayathribnaick] <[gayathri.bnaick@gmail.com]>
2024-07-13 20:13:02 +0530

    Move Exception Class into Folder

---
+++ FlowinglyTest/FlowinglyTest/Exceptions/ApiException.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+namespace FlowinglyTest.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public ApiException() { }
+        public ApiException(string message) : base(message) { }
+
+        public override string? StackTrace => string.Empty;
+    }
+}
--- FlowinglyTest/FlowinglyTest/Managers/ConvertXmlManager.cs
+++ FlowinglyTest/FlowinglyTest/Managers/ConvertXmlManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using FlowinglyTest.Exceptions;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace FlowinglyTest.Managers
